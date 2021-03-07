214ef556d9002cc5a355a7c6493421e176fd136d
m1lkweed <m1lkweed@users.noreply.github.com>
2021-03-07 17:28:16 -0500

    Update example.c
    Tabs

--- example.c
+++ example.c
@@ -1,18 +1,18 @@
 #include "logger.h"
 
 int main(){
-    log_level = ALL;
-    logfile = stdout;
-    logger(ALL,   "Message!");
-    logger(TRACE, "Message!");
-    logger(SPAM,  "Message!");
-    logger(DEBUG, "Message!");
-    logger(EXTRA, "Message!");
-    logger(INFO,  "Message!");
-    logger(ATTN,  "Message!");
-    logger(WARN,  "Message!");
-    logger(ERROR, "Message!");
-    logger(CRIT,  "Message!");
-    logger(FATAL, "Message!");
-    logger(NONE,  "Message!"); //Should never print
+	log_level = ALL;
+	logfile = stdout;
+	logger(ALL,   "Message!");
+	logger(TRACE, "Message!");
+	logger(SPAM,  "Message!");
+	logger(DEBUG, "Message!");
+	logger(EXTRA, "Message!");
+	logger(INFO,  "Message!");
+	logger(ATTN,  "Message!");
+	logger(WARN,  "Message!");
+	logger(ERROR, "Message!");
+	logger(CRIT,  "Message!");
+	logger(FATAL, "Message!");
+	logger(NONE,  "Message!"); //Should never print
 }
